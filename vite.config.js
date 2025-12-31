d659b4887336d22a79d56d636f70113d45c5e252
Frank_User#13 <frankrojas947@gmail.com>
2025-12-31 03:56:14 -0500

    first commit 10

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path';
 export default defineConfig({
-  root: '.',        // raíz del proyecto
+  root: '.',
   build: {
-    outDir: 'dist', // carpeta de salida
-    emptyOutDir: true
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        crearTarea: resolve(__dirname, 'crearTarea.html'),
+      }
+    }
   }
 });
