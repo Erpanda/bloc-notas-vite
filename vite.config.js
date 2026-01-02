7b41aa2252351746c1d3839256aba6ddb2de90a0
Frank_User#13 <frankrojas947@gmail.com>
2026-01-01 19:05:14 -0500

    actualization

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        home: resolve(__dirname, 'home.html'),
-        login: resolve(__dirname, 'login.html'),
-        register: resolve(__dirname, 'register.html'),
-        dashboard: resolve(__dirname, 'dashboard.html'),
-        crearTarea: resolve(__dirname, 'crearTarea.html'),
+        home: resolve(__dirname, 'views/home.html'),
+        login: resolve(__dirname, 'views/login.html'),
+        register: resolve(__dirname, 'views/register.html'),
+        dashboard: resolve(__dirname, 'views/dashboard.html'),
+        crearTarea: resolve(__dirname, 'views/crearTarea.html'),
       }
     }
   }
